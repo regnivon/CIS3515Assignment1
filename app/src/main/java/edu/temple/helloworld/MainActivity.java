2d3c3b56209bd26ff4431553f7e9a9e7ebf8fc3d
regnivon <qdnovinger@gmail.com>
2021-02-03 15:42:15 -0500

    Added a button which changes the welcome text when pressed

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -1,5 +1,8 @@
 package edu.temple.helloworld;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView text = findViewById(R.id.text_id);
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                text.setText(getString(R.string.clickString));
+            }
+        });
     }
 }
